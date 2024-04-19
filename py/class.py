8fa84f46d0ecaa60fcfd2a337979e31e5ee4bff5
vanTHkrab <vanth2548@gmail.com>
2024-04-19 11:40:40 +0700

    beta

---
+++ py/class.py
@@ -0,0 +1,9 @@
+class math:
+    def __init__(sit, month, year):
+        day = day
+        month = month
+        year = year
+    def add(x, y):
+        return x + y
+    
+        
---
+++ py/test.py
@@ -0,0 +1,4 @@
+class math():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
